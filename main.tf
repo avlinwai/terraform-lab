5f2e9f0d9c364bf2ec7b0a1eed1451ad6e9b396b
azlab11 <azlab11_cbe@WWLx393208.onmicrosoft.com>
2026-01-14 17:41:46 +0530

    Removed Secrets NEW

--- main.tf
+++ main.tf
@@ -1,35 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "520-rg"
-    storage_account_name = "storageavlin123"
-    container_name       = "terraform-state"
-    key                  = "jenkins-demo.tfstate"
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "520-JenkinsTerraformRG1"
-  location = "Italy North"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "JenkinsVNet"
-  address_space       = ["10.10.0.0/16"]
+  name                = var.vnet_name
+  address_space       = var.address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
